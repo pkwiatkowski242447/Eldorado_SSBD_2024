6dc011dc968d29f703f8e188576e2eb9aaf957c2
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-03-20 21:36:43 +0100

    "Dodanie zależności do pakietów spring-webmvc, jakarta-servlet-api; Konfiguracja wtyczek odpowiedzialnych za kompilacje projektu oraz wytworznie archiwum WAR; Utworzeniu testowego serwisu i kontrolera zawierającego metodę zwracającą ciąg znaków 'Hello World'"

---
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/config/WebConfig.java
@@ -0,0 +1,10 @@
+package pl.lodz.p.it.ssbd2024.ssbd03.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.lodz.p.it.ssbd2024.ssbd03")
+public class WebConfig {
+
+}
