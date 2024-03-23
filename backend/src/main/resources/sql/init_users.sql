9b9f133cbbb932c01b07162aa74f595ad12b8fa2
242415 Karaszewski Adam <242415@edu.p.lodz.pl>
2024-03-23 23:13:05 +0100

    Edycja skryptu inicjalizujaceg

--- backend/src/main/resources/sql/init_users.sql
+++ backend/src/main/resources/sql/init_users.sql
@@ -1,5 +1,22 @@
-CREATE DATABASE ssbd00;
+CREATE DATABASE ssbd03;
 CREATE USER ssbd03admin PASSWORD 'admin';
 CREATE USER ssbd03auth PASSWORD 'auth';
 CREATE USER ssbd03mok PASSWORD 'mok';
-GRANT ALL PRIVILEGES ON DATABASE ssbd00 TO ssbd03admin;
+CREATE USER ssbd03mop PASSWORD 'mop';
+GRANT ALL PRIVILEGES ON SCHEMA public TO ssbd03admin;
+GRANT ALL PRIVILEGES ON DATABASE ssbd03 TO ssbd03admin;
+\c ssbd03;
+CREATE TABLE testtable (
+                           id BIGINT NOT NULL,
+                           name VARCHAR(255) NOT NULL,
+                           age INTEGER NOT NULL,
+                           PRIMARY KEY (id)
+);
+
+CREATE TABLE testtable2 (
+                            id UUID NOT NULL,
+                            name VARCHAR(255) NOT NULL,
+                            age INTEGER NOT NULL,
+                            date DATE NOT NULL,
+                            PRIMARY KEY (id)
+);
