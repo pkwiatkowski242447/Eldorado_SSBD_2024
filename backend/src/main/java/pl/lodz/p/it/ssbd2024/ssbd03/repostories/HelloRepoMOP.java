0e11d861f928e3234f0657d84798acdb2a9f0b7f
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-03-24 22:25:15 +0100

    Dodanie i konfiguracja loggera

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/repostories/HelloRepoMOP.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/repostories/HelloRepoMOP.java
@@ -2,6 +2,8 @@ package pl.lodz.p.it.ssbd2024.ssbd03.repostories;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.ssbd2024.ssbd03.dbconfig.DatabaseConfigConstants;
@@ -10,12 +12,14 @@ import pl.lodz.p.it.ssbd2024.ssbd03.entities.HelloEntity2;
 @Repository
 public class HelloRepoMOP {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HelloRepoMOP.class);
+
     @PersistenceContext(unitName= DatabaseConfigConstants.MOP_PU)
     private EntityManager entityManager;
 
-    //FIXME problem z uprawnieniami w bazie
     @Transactional(DatabaseConfigConstants.TXM_MOP)
     public void save(HelloEntity2 ent) {
+        LOGGER.info("USING PU: " + entityManager.getEntityManagerFactory().getProperties().get("hibernate.ejb.persistenceUnitName"));
         entityManager.persist(ent);
     }
 }
