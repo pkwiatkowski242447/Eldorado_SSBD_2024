b1f95cd9be08a18b7a47d810ade7138b4bb2a9cf
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-04-03 20:23:30 +0200

    Dodanie pierwszej wersji konfiguracji puli polaczen z uzyciem HikariCP

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/dbconfig/mokPU/DataSourceMOKConfig.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/dbconfig/mokPU/DataSourceMOKConfig.java
@@ -1,5 +1,7 @@
 package pl.lodz.p.it.ssbd2024.ssbd03.dbconfig.mokPU;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,13 +24,28 @@ public class DataSourceMOKConfig {
     @Value("${jdbc.ssbd03.mok.password}")
     private String password;
 
+//    @Bean(DatabaseConfigConstants.DS_MOK)
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(driverClassName);
+//        dataSource.setUrl(url);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//        return dataSource;
+//    }
+
     @Bean(DatabaseConfigConstants.DS_MOK)
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName(driverClassName);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+
+        hikariConfig.setMaximumPoolSize(2);
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setPoolName("MOK_HIKARI_CP");
+
+        return new HikariDataSource(hikariConfig);
     }
 }
