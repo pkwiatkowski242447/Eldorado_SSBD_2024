7e743c3a42173937afbff339f396f23379da5b5a
216982 Janikowski Wadim <216982@edu.p.lodz.pl>
2024-04-06 11:58:39 +0200

    Modyfikacja skryptów SQL
    https://atlas.it.p.lodz.pl/jira/browse/SSBD202403-121

--- backend/src/main/resources/sql/init_struct.sql
+++ backend/src/main/resources/sql/init_struct.sql
@@ -3,7 +3,10 @@ CREATE USER ssbd03admin PASSWORD 'admin';
 CREATE USER ssbd03auth PASSWORD 'auth';
 CREATE USER ssbd03mok PASSWORD 'mok';
 CREATE USER ssbd03mop PASSWORD 'mop';
-\c ssbd03;
+\c ssbd03 postgres;
+GRANT ALL PRIVILEGES ON SCHEMA public TO ssbd03admin WITH GRANT OPTION
+GRANT ALL PRIVILEGES ON DATABASE ssbd03 TO ssbd03admin;
+\c ssbd03 ssbd03admin;
 CREATE TABLE testtable (
                            id BIGINT NOT NULL,
                            name VARCHAR(255) NOT NULL,
