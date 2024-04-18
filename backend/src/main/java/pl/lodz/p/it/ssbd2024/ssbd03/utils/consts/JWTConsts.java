02dc4c2c4323d4d0da57b433b2ad76f574fc68a9
242447 Piotr Kwiatkowski <242447@edu.p.lodz.pl>
2024-04-18 16:20:14 +0200

    Konfiguracja Spring Security - do przetestowania i ewentualnej zmiany przy implementacji przypadku użycia MOK.2

---
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/utils/consts/JWTConsts.java
@@ -0,0 +1,11 @@
+package pl.lodz.p.it.ssbd2024.ssbd03.utils.consts;
+
+public class JWTConsts {
+
+    public static final int SECRET_KEY_LENGTH = 256;
+
+    public static final String TOKEN_ISSUER = "Eldorado";
+
+    public static final String ACCOUNT_ID = "account_id";
+    public static final String USER_LEVELS = "user_levels";
+}
