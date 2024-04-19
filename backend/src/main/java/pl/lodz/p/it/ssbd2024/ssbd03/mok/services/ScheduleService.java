464275d4786afc3fb70ae40a7e0b0c1289254619
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-04-20 01:39:24 +0200

    Rozszerzenie implementacji automatycznego usuniecia niezweryfikowanego konta uzytkownika wraz z przypisanymi do tokenami; Dodanie obslugi wyjatkow dla metody usuwania konta

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/services/ScheduleService.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/services/ScheduleService.java
@@ -2,14 +2,19 @@ package pl.lodz.p.it.ssbd2024.ssbd03.mok.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.mok.Account;
+import pl.lodz.p.it.ssbd2024.ssbd03.exceptions.schedule.ScheduleBadProperties;
 import pl.lodz.p.it.ssbd2024.ssbd03.mok.facades.AccountMOKFacade;
+import pl.lodz.p.it.ssbd2024.ssbd03.mok.facades.TokenFacade;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.consts.mok.ScheduleConsts;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -18,32 +23,45 @@ public class ScheduleService {
 
     private final AccountMOKFacade accountMOKFacade;
 
+    private final TokenFacade tokenFacade;
+
+    @Value("${scheduler.not_verified_account_delete_time}")
+    private String deleteTime;
+
     @Autowired
-    public ScheduleService(AccountMOKFacade accountMOKFacade) {
+    public ScheduleService(AccountMOKFacade accountMOKFacade, TokenFacade tokenFacade) {
         this.accountMOKFacade = accountMOKFacade;
+        this.tokenFacade = tokenFacade;
     }
 
-    //TODO delay???????
-    @Scheduled(fixedRate = 1L, timeUnit = TimeUnit.MINUTES, initialDelay = 1L)
+    // FIXME delay???????
+    @Scheduled(fixedRate = 1L, timeUnit = TimeUnit.MINUTES, initialDelay = 0L)
     @Transactional(propagation = Propagation.REQUIRED)
-    public void deleteNotVerifiedAccount() {
-        log.info("Deleting unverified scheduler invoked");
+    public void deleteNotVerifiedAccount() throws ScheduleBadProperties {
+        log.info(ScheduleConsts.INVOKING_DELETE_ACCOUNTS_MESS);
 
         // Find not verified accounts
-        var inactiveAccountsOpt = accountMOKFacade.findAllAccountsMarkedForDeletion(1L, TimeUnit.MILLISECONDS);
+        Optional<List<Account>> inactiveAccountsOpt;
+        try {
+            inactiveAccountsOpt =
+                    accountMOKFacade.findAllAccountsMarkedForDeletion(Long.parseLong(deleteTime), TimeUnit.MINUTES);
+        } catch (NumberFormatException e) {
+            throw new ScheduleBadProperties(ScheduleConsts.BAD_PROP_FORMAT.formatted("not_verified_account_delete_time"));
+        }
 
         if (inactiveAccountsOpt.isEmpty()) {
-            log.info("No accounts");
+            log.info(ScheduleConsts.NO_ACCOUNTS_TO_DELETE_MESS);
             return;
         }
 
         List<Account> inactiveAccounts = inactiveAccountsOpt.get();
 
-        log.info("Accounts to delete: {}", inactiveAccounts);
+        log.info(ScheduleConsts.LIST_ACCOUNTS_TO_DELETE_IDS, inactiveAccounts.stream().map(Account::getId).toList());
 
-        // Delete accounts
-
-        inactiveAccounts.forEach(accountMOKFacade::remove);
+        // Delete accounts and linked tokens
+        inactiveAccounts.forEach(a -> {
+            tokenFacade.removeByAccount(a.getId());
+            accountMOKFacade.remove(a);
+        });
     }
-
 }
