1079dc1781ac71d4e5fde8eafa139abf9e762a75
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-04-20 16:33:28 +0200

    Dodanie konfiguracji wtyczki Javadoc oraz komentarzy dla klasy ReservationFacade

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mop/facades/ReservationFacade.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mop/facades/ReservationFacade.java
@@ -13,57 +13,102 @@ import java.util.UUID;
 
 import pl.lodz.p.it.ssbd2024.ssbd03.config.dbconfig.DatabaseConfigConstants;
 
+/**
+ * Implementation of AbstractFacade that provides CRUD operations
+ * on the Reservation class using the injected EntityManager implementation.
+ * @see Reservation
+ * @see jakarta.persistence.EntityManager
+ */
 @Repository
 public class ReservationFacade extends AbstractFacade<Reservation> {
 
     @PersistenceContext(unitName = DatabaseConfigConstants.MOP_PU)
     private EntityManager entityManager;
 
+    /**
+     * Invokes superclass constructor with Class object passed.
+     */
     public ReservationFacade() {
         super(Reservation.class);
     }
 
+    /**
+     * Returns injected EntityManager implementation.
+     * @return EntityManager implementation
+     */
     @Override
     protected EntityManager getEntityManager() {
         return this.entityManager;
     }
 
+    /**
+     * Invokes superclass create method on passed Reservation entity
+     * @param entity Reservation entity
+     */
     @Transactional
     @Override
     public void create(Reservation entity) {
         super.create(entity);
     }
 
+    /**
+     * Invokes superclass edit method on passed Reservation entity
+     * @param entity Reservation entity
+     */
     @Transactional
     @Override
     public void edit(Reservation entity) {
         super.edit(entity);
     }
 
+    /**
+     * Invokes superclass remove method on passed Reservation entity
+     * @param entity Reservation entity
+     */
     @Transactional
     @Override
     public void remove(Reservation entity) {
         super.remove(entity);
     }
 
+    /**
+     * Invokes superclass find method by passed entity id.
+     * @param id The UUID type identifier of the entity being searched for
+     * @return Entity, wrapped in Optional class, with identifiers equals to id param
+     */
     @Transactional
     @Override
     public Optional<Reservation> find(UUID id) {
         return super.find(id);
     }
 
+    /**
+     * Invokes superclass find with refreshing method by passed entity id.
+     * @param id The UUID type identifier of the entity being searched for
+     * @return Entity, wrapped in Optional class, with identifiers equals to id param
+     */
     @Transactional
     @Override
     public Optional<Reservation> findAndRefresh(UUID id) {
         return super.findAndRefresh(id);
     }
 
+    /**
+     * Invokes superclass find all method.
+     * @return All Reservation entities
+     */
     @Transactional
     @Override
     public List<Reservation> findAll() {
         return super.findAll();
     }
 
+    /**
+     * Returns all Reservation entities, based on NamedQuery defined on Reservation class, with pagination.
+     * @param page page number
+     * @param pageSize defines the maximum number of entities per page
+     * @return All Reservation entities from selected page
+     */
     @Transactional
     public List<Reservation> findAllWithPagination(int page, int pageSize) {
         var list = getEntityManager()
@@ -75,6 +120,12 @@ public class ReservationFacade extends AbstractFacade<Reservation> {
         return list;
     }
 
+    /**
+     * Returns all active Reservation entities, based on NamedQuery defined on Reservation class, with pagination.
+     * @param page page number
+     * @param pageSize defines the maximum number of entities per page
+     * @return All active Reservation entities from selected page
+     */
     @Transactional
     public List<Reservation> findActiveReservationsWithPagination(UUID clientId, int page, int pageSize) {
         var list = getEntityManager()
@@ -87,6 +138,12 @@ public class ReservationFacade extends AbstractFacade<Reservation> {
         return list;
     }
 
+    /**
+     * Returns all historical Reservation entities, based on NamedQuery defined on Reservation class, with pagination.
+     * @param page page number
+     * @param pageSize defines the maximum number of entities per page
+     * @return All historical Reservation entities from selected page
+     */
     @Transactional
     public List<Reservation> findHistoricalReservationsWithPagination(UUID clientId, int page, int pageSize) {
         var list = getEntityManager()
@@ -99,6 +156,14 @@ public class ReservationFacade extends AbstractFacade<Reservation> {
         return list;
     }
 
+    /**
+     * Returns all Reservation entities for the selected Sector, based on NamedQuery defined on Reservation class,
+     * with pagination.
+     * @param sectorId The UUID type identifier of the selected Sector
+     * @param page page number
+     * @param pageSize defines the maximum number of entities per page
+     * @return All Reservation entities for selected Sector and page
+     */
     @Transactional
     public List<Reservation> findSectorReservationsWithPagination(UUID sectorId, int page, int pageSize) {
         var list = getEntityManager()
@@ -111,6 +176,10 @@ public class ReservationFacade extends AbstractFacade<Reservation> {
         return list;
     }
 
+    /**
+     * Invokes superclass count method.
+     * @return Returns the total number of Reservation entities
+     */
     @Transactional
     @Override
     public int count() {
