612c8e22914d0b4f21c2f0c2dce48312b0a13d69
216982 Janikowski Wadim <216982@edu.p.lodz.pl>
2024-04-23 12:49:10 +0200

    Implementacja ponownego wysyłania maila do rejestracji

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/facades/TokenFacade.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/facades/TokenFacade.java
@@ -4,15 +4,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PersistenceException;
 import jakarta.persistence.TypedQuery;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Repository;
 import pl.lodz.p.it.ssbd2024.ssbd03.commons.AbstractFacade;
 import pl.lodz.p.it.ssbd2024.ssbd03.config.dbconfig.DatabaseConfigConstants;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.Token;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Repository
 public class TokenFacade extends AbstractFacade<Token> {
 
@@ -84,6 +88,19 @@ public class TokenFacade extends AbstractFacade<Token> {
 
         }
 
+    }
+    public List<Token> findByTokenType(Token.TokenType tokenType) {
+        try {
+            TypedQuery<Token> query = getEntityManager()
+                .createNamedQuery("Token.findByTokenType", Token.class)
+                .setParameter("tokenType", tokenType);
+            var list = query.getResultList();
+            refreshAll(list);
+            return list;
+        } catch (PersistenceException exception) {
+            log.error(exception.getMessage());
+            return new ArrayList<>();
+        }
     }
 
     public void removeByAccount(UUID accountId) {
--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/services/ScheduleService.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/mok/services/ScheduleService.java
@@ -7,12 +7,17 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import pl.lodz.p.it.ssbd2024.ssbd03.entities.Token;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.mok.Account;
 import pl.lodz.p.it.ssbd2024.ssbd03.exceptions.schedule.ScheduleBadProperties;
 import pl.lodz.p.it.ssbd2024.ssbd03.mok.facades.AccountMOKFacade;
 import pl.lodz.p.it.ssbd2024.ssbd03.mok.facades.TokenFacade;
 import pl.lodz.p.it.ssbd2024.ssbd03.utils.consts.mok.ScheduleConsts;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.providers.MailProvider;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -24,13 +29,16 @@ public class ScheduleService {
 
     private final TokenFacade tokenFacade;
 
+    private final MailProvider mailProvider;
+
     @Value("${scheduler.not_verified_account_delete_time}")
     private String deleteTime;
 
     @Autowired
-    public ScheduleService(AccountMOKFacade accountMOKFacade, TokenFacade tokenFacade) {
+    public ScheduleService(AccountMOKFacade accountMOKFacade, TokenFacade tokenFacade, MailProvider mailProvider) {
         this.accountMOKFacade = accountMOKFacade;
         this.tokenFacade = tokenFacade;
+        this.mailProvider = mailProvider;
     }
 
     @Scheduled(fixedRate = 1L, timeUnit = TimeUnit.HOURS, initialDelay = -1L)
@@ -59,6 +67,28 @@ public class ScheduleService {
         inactiveAccounts.forEach(a -> {
             tokenFacade.removeByAccount(a.getId());
             accountMOKFacade.remove(a);
+        });
+    }
+
+    @Scheduled(fixedRate = 1L, timeUnit = TimeUnit.HOURS, initialDelay = -1L)
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void resendConfirmationEmail() {
+        log.info(ScheduleConsts.INVOKING_RESEND_CONFIRMATION_EMAIL);
+
+        // Find all tokens of type REGISTER
+        List<Token> registerTokens = tokenFacade.findByTokenType(Token.TokenType.REGISTER);
+
+        registerTokens.forEach(token -> {
+            Account account = token.getAccount();
+            if (account.getCreationDate().isBefore(LocalDateTime.now().minus(12, ChronoUnit.HOURS))) {
+                String confirmationURL = "http://localhost:8080/api/v1/account/activate-account/" + token;
+                mailProvider.sendRegistrationConfirmEmail(account.getName(),
+                                                          account.getLastname(),
+                                                          account.getEmail(),
+                                                          confirmationURL,
+                                                          account.getAccountLanguage());
+                tokenFacade.removeByTypeAndAccount(Token.TokenType.REGISTER, account.getId());
+            }
         });
     }
 }
