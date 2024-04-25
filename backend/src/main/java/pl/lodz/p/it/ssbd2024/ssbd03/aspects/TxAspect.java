9d8d46a9a773a02cf985ffdcdee64aa628ca8fdc
242447 Piotr Kwiatkowski <242447@edu.p.lodz.pl>
2024-04-25 02:32:26 +0200

    Zmiany w konfiguracji aspektu. Wykorzystanie klas z pakiegu org.springframework.transaction oraz dodanie zależności do spring-aspects.

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/aspects/TxAspect.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/aspects/TxAspect.java
@@ -5,25 +5,18 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import pl.lodz.p.it.ssbd2024.ssbd03.transactions.TransactionKeyTracker;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 @Slf4j
 @Aspect
 @Component
-public class TxAspect {
+public class TxAspect extends TransactionInterceptor {
 
-    private final TransactionKeyTracker transactionKeyTracker;
 
     @Pointcut(value = "@annotation(org.springframework.transaction.annotation.Transactional) || @within(org.springframework.transaction.annotation.Transactional)")
     private void txPointcut() {}
-
-    @Autowired
-    public TxAspect(TransactionKeyTracker transactionKeyTracker) {
-        this.transactionKeyTracker = transactionKeyTracker;
-    }
 
     @Around(value = "txPointcut()")
     private Object aroundTxPointcut(ProceedingJoinPoint proceedingJoinPoint) {
@@ -31,15 +24,19 @@ public class TxAspect {
         Object result = new Object();
         try {
             try {
+                TransactionInfo transactionInfo = TransactionInterceptor.currentTransactionInfo();
                 message.append(proceedingJoinPoint.getSignature().getName());
                 message.append(" | Class: ").append(proceedingJoinPoint.getTarget().getClass().getSimpleName());
-                message.append(" | TransactionKey: ").append(transactionKeyTracker.getTransactionKey());
+                message.append(" | TransactionStatus before: ").append(transactionInfo != null ? transactionInfo.getTransactionStatus() : "NULL");
                 message.append(" | User: ").append(null != SecurityContextHolder.getContext().getAuthentication() ? SecurityContextHolder.getContext().getAuthentication().getName() : "--ANONYMOUS--");
             } catch (Exception e) {
                 log.error(" | Unexpected exception within aspect: ", e);
                 throw e;
             }
             result = proceedingJoinPoint.proceed();
+
+            TransactionInfo transactionInfo = TransactionInterceptor.currentTransactionInfo();
+            message.append(" | TransactionStatus after: ").append(transactionInfo != null ? transactionInfo.getTransactionStatus() : "NULL");
         } catch (Throwable e) {
             message.append(" | Thrown exception: ").append(e);
             log.error(message.toString(), e);
