19856d1490063d0a65b56dc812f3b285204257fc
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-05-04 23:24:20 +0200

    Przeniesienie wszystkiach adnotacji @PropertySource do jednej klasy konfiguracyjnej - ApplicationConfig

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/config/dbconfig/authPU/JpaAuthConfig.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/config/dbconfig/authPU/JpaAuthConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -14,7 +13,6 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource(value = {"classpath:application.properties"})
 @EnableJpaRepositories(
         value = DatabaseConfigConstants.JPA_PACKAGE_TO_SCAN,
         entityManagerFactoryRef = DatabaseConfigConstants.EMF_AUTH
