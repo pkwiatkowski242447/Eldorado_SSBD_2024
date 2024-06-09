7d6750de519376512aa356a1b15fefa16425cd2a
242391 Marta Glinka <242391@edu.p.lodz.pl>
2024-06-09 18:39:17 +0200

    Dodanie walidacji BeanValidation do klas: SectorCreateDTO, SectorModifyDTO.

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/commons/dto/mop/sectorDTO/SectorCreateDTO.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/commons/dto/mop/sectorDTO/SectorCreateDTO.java
@@ -1,5 +1,8 @@
 package pl.lodz.p.it.ssbd2024.ssbd03.commons.dto.mop.sectorDTO;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,6 +12,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.lodz.p.it.ssbd2024.ssbd03.aspects.logging.LoggerInterceptor;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.mop.Sector;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.consts.mop.SectorConsts;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.messages.mop.SectorMessages;
 
 @Getter
 @Setter
@@ -17,14 +22,28 @@ import pl.lodz.p.it.ssbd2024.ssbd03.entities.mop.Sector;
 @LoggerInterceptor
 public class SectorCreateDTO {
 
+    @Pattern(regexp = SectorConsts.SECTOR_NAME_PATTERN, message = SectorMessages.SECTOR_REGEX_NOT_MET)
+    @Size(min = SectorConsts.SECTOR_NAME_LENGTH, max = SectorConsts.SECTOR_NAME_LENGTH, message = SectorMessages.SECTOR_NAME_INVALID)
     @Schema(description = "The name of the sector", example = "B9", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
+
+    @NotNull(message = SectorMessages.SECTOR_TYPE_NULL)
+    @Enumerated(EnumType.STRING)
     @Schema(description = "The type of the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Sector.SectorType type;
+
+    @NotNull(message = SectorMessages.SECTOR_AVAILABLE_PLACES_NULL)
+    @PositiveOrZero(message = SectorMessages.SECTOR_AVAILABLE_PLACES_NEGATIVE)
     @Schema(description = "The maximum number of parking spots in the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer maxPlaces;
+
+    @NotNull(message = SectorMessages.SECTOR_WEIGHT_NULL)
+    @Min(value = SectorConsts.SECTOR_WEIGHT_MIN_WEIGHT, message = SectorMessages.SECTOR_WEIGHT_TOO_SMALL)
+    @Max(value = SectorConsts.SECTOR_WEIGHT_MAX_WEIGHT, message = SectorMessages.SECTOR_WEIGHT_TOO_LARGE)
     @Schema(description = "The weight of the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer weight;
+
+    @NotNull(message = SectorMessages.SECTOR_ACTIVE_NULL)
     @Schema(description = "Determines whether the sector is active", requiredMode = Schema.RequiredMode.REQUIRED)
     private Boolean active;
 
--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/commons/dto/mop/sectorDTO/SectorModifyDTO.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/commons/dto/mop/sectorDTO/SectorModifyDTO.java
@@ -1,5 +1,11 @@
 package pl.lodz.p.it.ssbd2024.ssbd03.commons.dto.mop.sectorDTO;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,6 +14,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.lodz.p.it.ssbd2024.ssbd03.aspects.logging.LoggerInterceptor;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.mop.Sector;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.consts.mop.SectorConsts;
+import pl.lodz.p.it.ssbd2024.ssbd03.utils.messages.mop.SectorMessages;
 
 import java.util.UUID;
 
@@ -17,10 +25,19 @@ import java.util.UUID;
 @LoggerInterceptor
 public class SectorModifyDTO extends SectorSignableDTO {
 
+    @NotNull(message = SectorMessages.SECTOR_TYPE_NULL)
+    @Enumerated(EnumType.STRING)
     @Schema(description = "The type of the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Sector.SectorType type;
+
+    @NotNull(message = SectorMessages.SECTOR_AVAILABLE_PLACES_NULL)
+    @PositiveOrZero(message = SectorMessages.SECTOR_AVAILABLE_PLACES_NEGATIVE)
     @Schema(description = "The maximum number of parking spots in the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer maxPlaces;
+
+    @NotNull(message = SectorMessages.SECTOR_WEIGHT_NULL)
+    @Min(value = SectorConsts.SECTOR_WEIGHT_MIN_WEIGHT, message = SectorMessages.SECTOR_WEIGHT_TOO_SMALL)
+    @Max(value = SectorConsts.SECTOR_WEIGHT_MAX_WEIGHT, message = SectorMessages.SECTOR_WEIGHT_TOO_LARGE)
     @Schema(description = "The weight of the sector", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer weight;
 
