b2141cbbc463ce399fb0e177c3d1db9514d26324
216982 Janikowski Wadim <216982@edu.p.lodz.pl>
2024-06-12 17:10:28 +0200

    Implementacja przypadku użycia MOP.5 w aplikacji SPA
    Implementacja przypadku użycia MOP.5 w aplikacji SPA

--- frontend/eldorado/src/router/routes.ts
+++ frontend/eldorado/src/router/routes.ts
@@ -16,8 +16,8 @@ import TwoFactorAuthPage from "@/pages/TwoFactorAuthPage.tsx";
 import AdminCreateUserPage from "@/pages/AdminCreateUserPage.tsx";
 import RestoreAccessPage from "@/pages/RestoreAccessPage.tsx";
 import RestoreTokenPage from "@/pages/RestoreTokenPage.tsx";
-import MyReservationsPage from "@/pages/MyReservationsPage.tsx";
-import AllReservationsPage from "@/pages/AllReservationsPage.tsx";
+import ParkingManagementPage from "@/pages/ParkingManagementPage.tsx";
+import {ParkingManagementInfoPage} from "@/pages/ParkingManagementInfoPage.tsx";
 
 
 export const PublicRoutes: RouteType[] = [
@@ -117,9 +117,13 @@ export const StaffRoutes: RouteType[] = [
         Component: ChangeUserLevelPage
     },
     {
-        path: Pathnames.staff.allReservations,
-        Component: AllReservationsPage
+        path: Pathnames.staff.parkingManagement,
+        Component: ParkingManagementPage
     },
+    {
+        path: Pathnames.staff.parkingManagementInfo,
+        Component: ParkingManagementInfoPage
+    }
 ]
 
 export const ClientRoutes: RouteType[] = [
@@ -138,9 +142,5 @@ export const ClientRoutes: RouteType[] = [
     {
         path: Pathnames.loggedIn.changeUserLevel,
         Component: ChangeUserLevelPage
-    },
-    {
-        path: Pathnames.client.myReservations,
-        Component: MyReservationsPage
     },
 ]
---
+++ frontend/eldorado/src/types/Parking.ts
@@ -0,0 +1,46 @@
+export enum SectorType {
+    UNCOVERED = "UNCOVERED",
+    COVERED = "COVERED",
+    UNDERGROUND = "UNDERGROUND"
+}
+
+export enum SectorStrategy {
+    LEAST_OCCUPIED = "LEAST_OCCUPIED",
+    MOST_OCCUPIED = "MOST_OCCUPIED",
+    LEAST_OCCUPIED_WEIGHTED = "LEAST_OCCUPIED_WEIGHTED"
+}
+export interface ParkingListType {
+    id: string;
+    city: string;
+    zipCode: string;
+    street: string;
+    strategy: SectorStrategy;
+    sectorTypes: SectorType[]
+}
+
+export interface ParkingType {
+    parkingId: string;
+    version: string;
+    city: string;
+    zipCode: string;
+    street: string;
+    strategy: SectorStrategy;
+    signature: string;
+}
+
+export interface CreateParkingType {
+    city: string;
+    zipCode: string;
+    street: string;
+    strategy: SectorStrategy;
+}
+
+export interface SectorListType {
+    id: string;
+    name:string;
+    active:boolean;
+    type: SectorType;
+    maxPlaces: number;
+    availablePlaces: number;    //todo change to occupiedSpaces
+    weight: number;
+}
