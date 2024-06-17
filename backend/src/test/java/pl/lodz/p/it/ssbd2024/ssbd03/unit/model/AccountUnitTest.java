5df8f017a1f20fa5e6808c49d472d9ba7b6a411b
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2024-06-18 01:13:43 +0200

    Poprawa testów jednostkowych, tak aby spełniały one wymagania testów mutacyjnych.

--- backend/src/test/java/pl/lodz/p/it/ssbd2024/ssbd03/unit/model/AccountUnitTest.java
+++ backend/src/test/java/pl/lodz/p/it/ssbd2024/ssbd03/unit/model/AccountUnitTest.java
@@ -1,14 +1,14 @@
 package pl.lodz.p.it.ssbd2024.ssbd03.unit.model;
 
-
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import pl.lodz.p.it.ssbd2024.ssbd03.entities.mok.Account;
+import pl.lodz.p.it.ssbd2024.ssbd03.entities.mok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountUnitTest {
 
@@ -20,18 +20,68 @@ public class AccountUnitTest {
     private static final String ACCOUNT_EMAIL_NO_1 = "accountNo1@email.com";
     private static final String ACCOUNT_PHONE_NUMBER_NO_1 = "000000000";
 
+    private static final int UNSUCCESSFUL_LOGIN_COUNTER = 2;
+    private static final LocalDateTime LAST_UNSUCCESSFUL_LOGIN_TIME = LocalDateTime.now().plusHours(12);
+    private static final String LAST_UNSUCCESSFUL_LOGIN_IP = "172.16.0.1";
+    private static final LocalDateTime LAST_SUCCESSFUL_LOGIN_TIME = LocalDateTime.now().minusHours(12);
+    private static final String LAST_SUCCESSFUL_LOGIN_IP = "192.168.0.1";
+
+    private static final String LOGIN_NO1 = "ExampleLogin";
+    private static final String PASSWORD_NO1 = "ExamplePassword";
+    private static final String FIRST_NAME_NO1 = "Firstname";
+    private static final String LAST_NAME_NO1 = "Lastname";
+    private static final String EMAIL_NO1 = "email@sb.pl";
+    private static final String PHONE_NUMBER_NO1 = "123456789";
+    private static final String ACCOUNT_LANGUAGE_NO1 = "PL";
+
+    private static Account exampleAccount;
+    private static Set<UserLevel> userLevels = new HashSet<>();
+
+    @BeforeAll
+    public static void setUp() {
+        exampleAccount = new Account(LOGIN_NO1,
+                PASSWORD_NO1,
+                FIRST_NAME_NO1,
+                LAST_NAME_NO1,
+                EMAIL_NO1,
+                PHONE_NUMBER_NO1);
+
+        exampleAccount.blockAccount(false);
+        exampleAccount.setAccountLanguage(ACCOUNT_LANGUAGE_NO1);
+
+        UserLevel client = new Client();
+        client.setAccount(exampleAccount);
+
+        UserLevel admin = new Admin();
+        admin.setAccount(exampleAccount);
+
+        userLevels.add(client);
+        userLevels.add(admin);
+
+        exampleAccount.addUserLevel(client);
+        exampleAccount.addUserLevel(admin);
+
+        ActivityLog activityLog = new ActivityLog();
+        activityLog.setUnsuccessfulLoginCounter(UNSUCCESSFUL_LOGIN_COUNTER);
+        activityLog.setLastSuccessfulLoginTime(LAST_SUCCESSFUL_LOGIN_TIME);
+        activityLog.setLastSuccessfulLoginIp(LAST_SUCCESSFUL_LOGIN_IP);
+        activityLog.setLastUnsuccessfulLoginTime(LAST_UNSUCCESSFUL_LOGIN_TIME);
+        activityLog.setLastUnsuccessfulLoginIp(LAST_UNSUCCESSFUL_LOGIN_IP);
+
+        exampleAccount.setActivityLog(activityLog);
+    }
+
     //ACTIVITY LOG DATA
 
-
     @Test
-    void getActivityLogForAccountWithNotNullActivityLogPositiveTest() {
+    public void getActivityLogForAccountWithNotNullActivityLogPositiveTest() {
         Account testAccount1 = new Account(ACCOUNT_LOGIN_NO_1, ACCOUNT_PASSWORD_NO_1, ACCOUNT_NAME_NO_1, ACCOUNT_LASTNAME_NO_1,
                 ACCOUNT_EMAIL_NO_1, ACCOUNT_PHONE_NUMBER_NO_1);
 
-        Assertions.assertNotNull(testAccount1.getActivityLog());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastSuccessfulLoginIp());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
+        assertNotNull(testAccount1.getActivityLog());
+        assertNull(testAccount1.getActivityLog().getLastSuccessfulLoginIp());
+        assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
+        assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
 
         LocalDateTime lastSuccessfulLoginTimeNo1 = LocalDateTime.of(2002, 10, 12, 10, 20, 30);
         LocalDateTime lastUnsuccessfulLoginTimeNo1 = LocalDateTime.of(2002, 10, 12, 9, 20, 30);
@@ -45,28 +95,28 @@ public class AccountUnitTest {
         testAccount1.getActivityLog().setLastSuccessfulLoginIp(lastSuccessfulLoginIPNo1);
         testAccount1.getActivityLog().setLastUnsuccessfulLoginIp(lastUnsuccessfulLoginIPNo1);
 
-        Assertions.assertNotNull(testAccount1.getActivityLog());
-        Assertions.assertEquals(lastSuccessfulLoginTimeNo1, testAccount1.getActivityLog().getLastSuccessfulLoginTime());
-        Assertions.assertEquals(lastSuccessfulLoginIPNo1, testAccount1.getActivityLog().getLastSuccessfulLoginIp());
-        Assertions.assertEquals(lastUnsuccessfulLoginTimeNo1, testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
-        Assertions.assertEquals(lastUnsuccessfulLoginIPNo1, testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
+        assertNotNull(testAccount1.getActivityLog());
+        assertEquals(lastSuccessfulLoginTimeNo1, testAccount1.getActivityLog().getLastSuccessfulLoginTime());
+        assertEquals(lastSuccessfulLoginIPNo1, testAccount1.getActivityLog().getLastSuccessfulLoginIp());
+        assertEquals(lastUnsuccessfulLoginTimeNo1, testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
+        assertEquals(lastUnsuccessfulLoginIPNo1, testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
     }
 
     @Test
-    void getActivityLogForAccountWithNotNullActivityLogPositiveLog() {
+    public void getActivityLogForAccountWithNotNullActivityLogPositiveLog() {
         Account testAccount1 = new Account(ACCOUNT_LOGIN_NO_1, ACCOUNT_PASSWORD_NO_1, ACCOUNT_NAME_NO_1, ACCOUNT_LASTNAME_NO_1,
                 ACCOUNT_EMAIL_NO_1, ACCOUNT_PHONE_NUMBER_NO_1);
 
         testAccount1.setActivityLog(null);
 
-        Assertions.assertNotNull(testAccount1.getActivityLog());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastSuccessfulLoginIp());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
-        Assertions.assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
+        assertNotNull(testAccount1.getActivityLog());
+        assertNull(testAccount1.getActivityLog().getLastSuccessfulLoginIp());
+        assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginTime());
+        assertNull(testAccount1.getActivityLog().getLastUnsuccessfulLoginIp());
     }
 
     @Test
-    void isEnabledReturnsTrueTest() {
+    public void isEnabledReturnsTrueTest() {
         Account testAccount1 = new Account(ACCOUNT_LOGIN_NO_1, ACCOUNT_PASSWORD_NO_1, ACCOUNT_NAME_NO_1, ACCOUNT_LASTNAME_NO_1,
                 ACCOUNT_EMAIL_NO_1, ACCOUNT_PHONE_NUMBER_NO_1);
 
@@ -87,7 +137,7 @@ public class AccountUnitTest {
     }
 
     @Test
-    void isEnabledReturnsFalseTest() {
+    public void isEnabledReturnsFalseTest() {
         Account testAccount1 = new Account(ACCOUNT_LOGIN_NO_1, ACCOUNT_PASSWORD_NO_1, ACCOUNT_NAME_NO_1, ACCOUNT_LASTNAME_NO_1,
                 ACCOUNT_EMAIL_NO_1, ACCOUNT_PHONE_NUMBER_NO_1);
 
@@ -98,12 +148,48 @@ public class AccountUnitTest {
     }
 
     @Test
-    void equalsReturnsFalseTest() {
+    public void equalsReturnsFalseTest() {
         Account testAccount1 = new Account(ACCOUNT_LOGIN_NO_1, ACCOUNT_PASSWORD_NO_1, ACCOUNT_NAME_NO_1, ACCOUNT_LASTNAME_NO_1,
                 ACCOUNT_EMAIL_NO_1, ACCOUNT_PHONE_NUMBER_NO_1);
 
         assertFalse(testAccount1.equals(null));
         assertFalse(testAccount1.equals("String"));
+    }
 
+    // Added tests
+
+    @Test
+    public void accountNoArgsConstructorTestPositive() {
+        Account testAccount = new Account();
+        assertNotNull(testAccount);
+    }
+
+    @Test
+    public void accountAllGettersTestPositive() {
+        assertEquals(exampleAccount.getLogin(), LOGIN_NO1);
+        assertEquals(exampleAccount.getPassword(), PASSWORD_NO1);
+        assertEquals(exampleAccount.getEmail(), EMAIL_NO1);
+        assertEquals(exampleAccount.getName(), FIRST_NAME_NO1);
+        assertEquals(exampleAccount.getLastname(), LAST_NAME_NO1);
+        assertEquals(exampleAccount.getPhoneNumber(), PHONE_NUMBER_NO1);
+        assertEquals(exampleAccount.getAccountLanguage(), ACCOUNT_LANGUAGE_NO1);
+
+        assertFalse(exampleAccount.getSuspended());
+        assertFalse(exampleAccount.getActive());
+        assertTrue(exampleAccount.getBlocked());
+        assertTrue(exampleAccount.getTwoFactorAuth());
+
+        assertNotNull(exampleAccount.getBlockedTime());
+
+        assertFalse(exampleAccount.getUserLevels().isEmpty());
+        assertEquals(2, exampleAccount.getUserLevels().size());
+        assertTrue(exampleAccount.getUserLevels().stream().anyMatch(userLevel -> userLevel instanceof Client));
+        assertTrue(exampleAccount.getUserLevels().stream().anyMatch(userLevel -> userLevel instanceof Admin));
+
+        assertEquals(exampleAccount.getActivityLog().getUnsuccessfulLoginCounter(), UNSUCCESSFUL_LOGIN_COUNTER);
+        assertEquals(exampleAccount.getActivityLog().getLastSuccessfulLoginTime(), LAST_SUCCESSFUL_LOGIN_TIME);
+        assertEquals(exampleAccount.getActivityLog().getLastSuccessfulLoginIp(), LAST_SUCCESSFUL_LOGIN_IP);
+        assertEquals(exampleAccount.getActivityLog().getLastUnsuccessfulLoginTime(), LAST_UNSUCCESSFUL_LOGIN_TIME);
+        assertEquals(exampleAccount.getActivityLog().getLastUnsuccessfulLoginIp(), LAST_UNSUCCESSFUL_LOGIN_IP);
     }
 }
--- backend/src/test/java/pl/lodz/p/it/ssbd2024/ssbd03/unit/model/ActivityLogUnitTest.java
+++ backend/src/test/java/pl/lodz/p/it/ssbd2024/ssbd03/unit/model/ActivityLogUnitTest.java
@@ -4,19 +4,135 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.lodz.p.it.ssbd2024.ssbd03.entities.mok.ActivityLog;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ActivityLogUnitTest {
 
     private ActivityLog activityLog;
+
+    private final int unsuccessfulLoginCounter = 2;
+    private final LocalDateTime lastUnsuccessfulLoginTime = LocalDateTime.now().plusHours(12);
+    private final String lastUnsuccessfulLoginIp = "172.16.0.1";
+    private final LocalDateTime lastSuccessfulLoginTime = LocalDateTime.now().minusHours(12);
+    private final String lastSuccessfulLoginIp = "192.168.0.1";
+
     @BeforeEach
     public void setUp() {
         activityLog = new ActivityLog();
-
+        activityLog.setUnsuccessfulLoginCounter(this.unsuccessfulLoginCounter);
+        activityLog.setLastUnsuccessfulLoginTime(this.lastUnsuccessfulLoginTime);
+        activityLog.setLastUnsuccessfulLoginIp(this.lastUnsuccessfulLoginIp);
+        activityLog.setLastSuccessfulLoginTime(this.lastSuccessfulLoginTime);
+        activityLog.setLastSuccessfulLoginIp(this.lastSuccessfulLoginIp);
     }
+
     @Test
-    public void testToString() {
+    public void activityLogNoArgsConstructorTestPositive() {
+        ActivityLog testActivityLog = new ActivityLog();
+        assertNotNull(testActivityLog);
+    }
+
+    @Test
+    public void activityLogAllGettersTestPositive() {
+        assertEquals(activityLog.getUnsuccessfulLoginCounter(), this.unsuccessfulLoginCounter);
+        assertEquals(activityLog.getLastUnsuccessfulLoginTime(), this.lastUnsuccessfulLoginTime);
+        assertEquals(activityLog.getLastUnsuccessfulLoginIp(), this.lastUnsuccessfulLoginIp);
+        assertEquals(activityLog.getLastSuccessfulLoginTime(), this.lastSuccessfulLoginTime);
+        assertEquals(activityLog.getLastSuccessfulLoginIp(), this.lastSuccessfulLoginIp);
+    }
+
+    @Test
+    public void activityLogUnsuccessfulSetLoginCounterTestPositive() {
+        int unsuccessfulLoginCounterBefore = activityLog.getUnsuccessfulLoginCounter();
+        int newUnsuccessfulLoginCounter = 3;
+
+        activityLog.setUnsuccessfulLoginCounter(newUnsuccessfulLoginCounter);
+
+        int unsuccessfulLoginCounterAfter = activityLog.getUnsuccessfulLoginCounter();
+
+        assertNotEquals(unsuccessfulLoginCounterBefore, newUnsuccessfulLoginCounter);
+        assertNotEquals(unsuccessfulLoginCounterBefore, unsuccessfulLoginCounterAfter);
+        assertEquals(newUnsuccessfulLoginCounter, unsuccessfulLoginCounterAfter);
+    }
+
+    @Test
+    public void activityLogSetUnsuccessfulLoginTimeTestPositive() {
+        LocalDateTime lastUnsuccessfulLoginTimeBefore = activityLog.getLastUnsuccessfulLoginTime();
+        LocalDateTime newLastUnsuccessfulLoginTime = LocalDateTime.now();
+
+        assertNotNull(lastUnsuccessfulLoginTimeBefore);
+        assertNotNull(newLastUnsuccessfulLoginTime);
+
+        activityLog.setLastUnsuccessfulLoginTime(newLastUnsuccessfulLoginTime);
+
+        LocalDateTime lastUnsuccessfulLoginTimeAfter = activityLog.getLastUnsuccessfulLoginTime();
+        assertNotNull(lastUnsuccessfulLoginTimeAfter);
+
+        assertNotEquals(lastUnsuccessfulLoginTimeBefore, newLastUnsuccessfulLoginTime);
+        assertNotEquals(lastUnsuccessfulLoginTimeBefore, lastUnsuccessfulLoginTimeAfter);
+        assertEquals(newLastUnsuccessfulLoginTime, lastUnsuccessfulLoginTimeAfter);
+    }
+
+    @Test
+    public void activityLogSetUnsuccessfulLoginIpTestPositive() {
+        String lastUnsuccessfulLoginIpBefore = activityLog.getLastSuccessfulLoginIp();
+        String newLastUnsuccessfulLoginIp = "10.16.0.0";
+
+        assertNotNull(lastUnsuccessfulLoginIpBefore);
+        assertNotNull(newLastUnsuccessfulLoginIp);
+
+        activityLog.setLastSuccessfulLoginIp(newLastUnsuccessfulLoginIp);
+
+        String lastUnsuccessfulLoginIpAfter = activityLog.getLastSuccessfulLoginIp();
+
+        assertNotNull(lastUnsuccessfulLoginIpAfter);
+
+        assertNotEquals(lastUnsuccessfulLoginIpBefore, newLastUnsuccessfulLoginIp);
+        assertNotEquals(lastUnsuccessfulLoginIpBefore, lastUnsuccessfulLoginIpAfter);
+        assertEquals(newLastUnsuccessfulLoginIp, lastUnsuccessfulLoginIpAfter);
+    }
+
+    @Test
+    public void activityLogSetSuccessfulLoginTimeTestPositive() {
+        LocalDateTime lastSuccessfulLoginTimeBefore = activityLog.getLastSuccessfulLoginTime();
+        LocalDateTime newLastSuccessfulLoginTime = LocalDateTime.now();
+
+        assertNotNull(lastSuccessfulLoginTimeBefore);
+        assertNotNull(newLastSuccessfulLoginTime);
+
+        activityLog.setLastSuccessfulLoginTime(newLastSuccessfulLoginTime);
+
+        LocalDateTime lastSuccessfulLoginTimeAfter = activityLog.getLastSuccessfulLoginTime();
+        assertNotNull(lastSuccessfulLoginTimeAfter);
+
+        assertNotEquals(lastSuccessfulLoginTimeBefore, newLastSuccessfulLoginTime);
+        assertNotEquals(lastSuccessfulLoginTimeBefore, lastSuccessfulLoginTimeAfter);
+        assertEquals(newLastSuccessfulLoginTime, lastSuccessfulLoginTimeAfter);
+    }
+
+    @Test
+    public void activityLogSetSuccessfulLoginIpTestPositive() {
+        String lastSuccessfulLoginIpBefore = activityLog.getLastSuccessfulLoginIp();
+        String newLastSuccessfulLoginIp = "10.31.255.0";
+
+        assertNotNull(lastSuccessfulLoginIpBefore);
+        assertNotNull(newLastSuccessfulLoginIp);
+
+        activityLog.setLastSuccessfulLoginIp(newLastSuccessfulLoginIp);
+
+        String lastSuccessfulLoginIpAfter = activityLog.getLastSuccessfulLoginIp();
+
+        assertNotNull(lastSuccessfulLoginIpAfter);
+
+        assertNotEquals(lastSuccessfulLoginIpBefore, newLastSuccessfulLoginIp);
+        assertNotEquals(lastSuccessfulLoginIpBefore, lastSuccessfulLoginIpAfter);
+        assertEquals(newLastSuccessfulLoginIp, lastSuccessfulLoginIpAfter);
+    }
+
+    @Test
+    public void activityLogToStringTestPositive() {
         String result = activityLog.toString();
         assertNotNull(result);
         assertFalse(result.isEmpty());
