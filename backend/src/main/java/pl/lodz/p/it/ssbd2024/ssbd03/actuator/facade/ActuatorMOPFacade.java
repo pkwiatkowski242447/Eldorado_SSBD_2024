6299b1e64b7f001ff08eeeb34f9cd83ea512b9f2
Michał Piestrzeniewicz <michal.p2608@wp.pl>
2024-10-31 18:22:45 +0100

    Add liviness/readiness impl

---
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/actuator/facade/ActuatorMOPFacade.java
@@ -0,0 +1,4 @@
+package pl.lodz.p.it.ssbd2024.ssbd03.actuator.facade;
+
+public class ActuatorMOKFacade {
+}
---
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/actuator/service/ActuatorService.java
@@ -0,0 +1,4 @@
+package pl.lodz.p.it.ssbd2024.ssbd03.actuator.controllers;
+
+public class ActuatorService {
+}
