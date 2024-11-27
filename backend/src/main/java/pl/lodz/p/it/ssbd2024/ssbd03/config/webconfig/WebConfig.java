bb8c0fd8a04556dffca2165658e85c1ca655348b
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2024-11-27 15:33:29 +0100

    Changed imports for some property files.

--- backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/config/webconfig/WebConfig.java
+++ backend/src/main/java/pl/lodz/p/it/ssbd2024/ssbd03/config/webconfig/WebConfig.java
@@ -31,21 +31,16 @@ import java.util.List;
 
 @EnableWebMvc
 @Configuration
-//@EnableAutoConfiguration(exclude = {
-//        DataSourceAutoConfiguration.class,
-//        DataSourceTransactionManagerAutoConfiguration.class,
-//        HibernateJpaAutoConfiguration.class
-//})
 @ComponentScan({"pl.lodz.p.it.ssbd2024.ssbd03", "org.springdoc"})
 @PropertySource(value = {
         "classpath:application.properties",
-        //"file:/usr/local/tomcat/config/mail.properties",
-        "classpath:properties/mail.properties",
+        "file:/usr/local/tomcat/config/mail.properties",
+        // "classpath:properties/mail.properties",
         "classpath:properties/urls.properties",
         "classpath:properties/consts.properties",
         "classpath:properties/retry.properties",
-        // "file:/usr/local/tomcat/config/key.properties"\
-        "classpath:properties/key.properties"
+        "file:/usr/local/tomcat/config/key.properties"
+        // "classpath:properties/key.properties"
 })
 @EnableAsync
 @EnableRetry
